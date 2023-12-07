bc1f1a861b6866d9815f2267b16b510c282bbdbe
石明帅 <62270889+Shi-Ming-Shuai@users.noreply.github.com>
2023-12-07 15:38:05 +0800

    更新 index 测试

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import {
   propsModule,
   styleModule,
   eventListenersModule,
+  as,
   h
 } from "snabbdom";
 
 const patch = init([
+  // 大我的哇
   // Init patch function with chosen modules
   classModule, // makes it easy to toggle classes
   propsModule, // for setting properties on DOM elements
@@ -23,7 +25,7 @@ const vnode = h('ul', {}, [
   h('li', {key:3}, 'c')
 ])
 patch(container, vnode)
-
+const 生答案所多= 213
 const vnode2 = h('div', {}, [
   h('li', {key:2}, 'b'),
   h('li', {key:1}, 'a'),
